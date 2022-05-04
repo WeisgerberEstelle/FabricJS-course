1ece2247380b024fb896f4cb72b051d7fd45e313
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2022-05-04 12:20:12 +0200

    added toggle mode drawing and pan

--- index.js
+++ index.js
@@ -16,29 +16,68 @@ function setBackground(url, canvas) {
   });
 }
 
+function toggleMode(mode) {
+  if (mode === modes.pan) {
+    if (currentMode === modes.pan) {
+      currentMode = "";
+    } else {
+      currentMode = modes.pan;
+      canvas.isDrawingMode = false;
+      canvas.renderAll();
+    }
+  } else if (mode === modes.drawing) {
+    if (currentMode === modes.drawing) {
+      currentMode = "";
+      canvas.isDrawingMode = false;
+      canvas.renderAll();
+    } else {
+      currentMode = modes.drawing;
+      canvas.isDrawingMode = true;
+      canvas.renderAll();
+    }
+  }
+  console.log(currentMode);
+}
+
+function setPanEvents(canvas) {
+  canvas.on("mouse:move", (event) => {
+    //console.log();
+    if (mousePressed && currentMode === modes.pan) {
+      canvas.setCursor("grab");
+      canvas.renderAll();
+      const mEvent = event.e;
+      const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
+      canvas.relativePan(delta);
+    } 
+  });
+
+  canvas.on("mouse:down", (event) => {
+    mousePressed = true;
+    if (mousePressed && currentMode === modes.pan) {
+      canvas.setCursor("crosshair");
+      canvas.renderAll();
+    }
+  });
+
+  canvas.on("mouse:up", (event) => {
+    mousePressed = false;
+    canvas.setCursor("default");
+    canvas.renderAll();
+  });
+}
+
 const canvas = initCanvas("canvas");
 let mousePressed = false;
+
+let currentMode;
+const modes = {
+  pan: "pan",
+  drawing: "drawing",
+};
 
 setBackground(
   "https://www.consoglobe.com/wp-content/uploads/2021/11/chat-maison_2079791452_ban.jpg",
   canvas
 );
 
-//mouse:over
-
-canvas.on("mouse:move", (event) => {
-  //console.log('hi');
-  if (mousePressed) {
-    const mEvent = event.e;
-    const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
-    canvas.relativePan(delta);
-  }
-});
-
-canvas.on("mouse:down", (event) => {
-  mousePressed = true;
-});
-
-canvas.on("mouse:up", (event) => {
-  mousePressed = false;
-});
+setPanEvents(canvas);
